c5080315ee3c3ab5c48952a11d20028228a9524c
dadasovmurad <dadashov243@gmail.com>
2025-06-11 10:36:40 +0400

    .

---
+++ src/PrepareToInterview.Application/Abstractions/Services/IQuestionService.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using PrepareToInterview.Application.DTOs.Question;
+using PrepareToInterview.Application.Results;
+
+namespace PrepareToInterview.Application.Abstractions.Services
+{
+    public interface IQuestionService
+    {
+        Task<Result> CreateAsync();
+        Task<DataResult<QuestionDto>> GetAllAsync();
+        Task<Dat>
+    }
+}
---
+++ src/PrepareToInterview.Persistence/Services/QuestionService.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using PrepareToInterview.Application.Abstractions.Services;
+
+namespace PrepareToInterview.Persistence.Services
+{
+    public class QuestionService : IQuestionService
+    {
+
+    }
+}
